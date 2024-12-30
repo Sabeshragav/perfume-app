07edd9f230b07960d5a50be94f413f4bfd6d17ac
Sabeshragav <sabeshragav289@gmail.com>
2024-12-30 10:31:20 +0530

    final

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,15 +1,20 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  price: Number,
-  sizes: [String],
-  images: [String],
-  rating: { type: Number, default: 0 }, // Add rating field
-  madeFrom: String, // Add madeFrom field (e.g., material or ingredients)
-  collection: Number, // Add category (e.g., perfume, skincare, etc.)
-  brand: String, // Add brand field
-});
+const productSchema = new mongoose.Schema(
+  {
+    name: String,
+    description: String,
+    price: Number,
+    sizes: [String],
+    images: [String],
+    rating: { type: Number, default: 0 }, // Add rating field
+    madeFrom: String, // Add madeFrom field (e.g., material or ingredients)
+    collection: Number, // Add category (e.g., perfume, skincare, etc.)
+    brand: String, // Add brand field
+  },
+  {
+    suppressReservedKeysWarning: true,
+  }
+);
 
 module.exports = mongoose.model("Product", productSchema);
